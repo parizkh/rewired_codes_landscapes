12de1c5442028efbd5784f3ef74dd7668d46a3ec
Carlos Marti <carlos199m1@gmail.com>
2023-05-23 15:33:51 -0400

    script update

--- GB1/05_landscape_visualizations/4_plot_top_genotypes.sh
+++ GB1/05_landscape_visualizations/4_plot_top_genotypes.sh
@@ -1,9 +1,22 @@
 cmd="conda activate gpmap; python plot_filt_landscape.py"
 qsub="qsub -cwd -l mem_free=8G"
 
-codes=`cut -f 1 -d ','  ../genetic_codes/genetic_codes.csv | grep -v code`
+code="Standard"
+echo "$cmd $code 2 1 3" | $qsub -N p$code
+echo "$cmd $code 3 1 2" | $qsub -N p$code
 
-for code in $codes
-do
-	echo "$cmd $code $filt" | $qsub -N $filt.$code 
-done
+code="9002"
+echo "$cmd $code 1 2 3" | $qsub -N p$code
+echo "$cmd $code 3 2 1" | $qsub -N p$code
+
+code="73213"
+echo "$cmd $code 2 1 3" | $qsub -N p$code
+echo "$cmd $code 2 3 1" | $qsub -N p$code
+
+code="5521"
+echo "$cmd $code 1 2 3" | $qsub -N p$code
+
+code="6037"
+echo "$cmd $code 1 2 3" | $qsub -N p$code
+echo "$cmd $code 3 2 1" | $qsub -N p$code
+
